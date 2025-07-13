784d6e63cc16f62889f6958bf34909ce93706335
Shubham shukla <shubhushukla586@gmail.com>
2025-07-13 23:38:01 +0530

    Add solutions for four LeetCode problems
    Added C++ solutions for the following problems: Maximum Matching of Players With Trainers (2410), Reverse Pairs (493), Container With Most Water (11), and Largest Number (179). Each file implements the respective algorithm for the problem.

---
+++ Medium/ContainerWithMostWater_11.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int i = 0, j = n-1;
+        
+        int water = 0;
+        while(i<j) {
+            
+            //start from the smallest one and calculate water
+            int h = min(height[i], height[j]);
+            int w = j-i;
+            int area = h*w;
+            water = max(water, area);
+            
+            if(height[i] < height[j])
+                i++;
+            else
+                j--;
+        }
+        
+        return water;
+    }
+};
