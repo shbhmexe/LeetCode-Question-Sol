a8442fdb91c7c8fce6ade48f6cb6f6d970129c66
Shubham shukla <shubhushukla586@gmail.com>
2025-07-26 23:44:16 +0530

    Daily Question
    Daily Question Leetcode

---
+++ Daily Questions/Easy/MaximumUniqueSubarraySumAfterDeletion_3487.cpp
@@ -0,0 +1,26 @@
+//Question 246
+
+//Approach-1 (Using set)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+        unordered_set<int> st;
+
+        int sum = 0;
+
+        int maxNeg = INT_MIN;
+
+        for(int &num : nums) {
+            if(num <= 0) {
+                maxNeg = max(maxNeg, num);
+            } else if(!st.count(num)) {
+                sum += num;
+                st.insert(num);
+            }
+        }
+        
+        return sum == 0 ? maxNeg : sum;
+    }
+};
