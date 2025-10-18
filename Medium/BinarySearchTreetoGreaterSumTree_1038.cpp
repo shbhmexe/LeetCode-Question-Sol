25421edf8117fbe39c3abb55791ebfb4a1875e98
Shubham shukla <shubhushukla586@gmail.com>
2025-10-18 23:57:06 +0530

    Add solutions for LeetCode problems 1038, 3003, and 3397
    Added C++ implementations for three LeetCode problems: Binary Search Tree to Greater Sum Tree (1038), Maximize the Number of Partitions After Operations (3003), and Maximum Number of Distinct Elements After Operations (3397). Each solution includes comments on approach and time/space complexity.

---
+++ Medium/BinarySearchTreetoGreaterSumTree_1038.cpp
@@ -0,0 +1,41 @@
+//Question 503
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+//Simple recursive approach
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+
+    void solve(TreeNode* root, int &sum) {
+        if(!root) { //root == NULL
+            return;
+        }
+
+        
+        solve(root->right, sum); //RIGHT is covered
+        
+        sum += root->val;
+
+        root->val = sum;
+
+        solve(root->left, sum); //Covergin the left
+    }
+
+    TreeNode* bstToGst(TreeNode* root) {
+        int sum = 0;
+
+        solve(root, sum);
+
+        return root;
+    }
+};
