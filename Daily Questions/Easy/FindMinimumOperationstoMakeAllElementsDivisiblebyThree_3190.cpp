6f587f7389bf96f57880381c75a45abe2716227a
shbhmexe <shubhushukla586@gmail.com>
2025-11-23 21:24:44 +0530

    Add solutions for three LeetCode problems
    Added C++ solutions for 'Find Minimum Operations to Make All Elements Divisible by Three' (Easy), 'Greatest Sum Divisible by Three' (Medium), and 'Most Stones Removed with Same Row or Column' (Medium). Each file contains a different algorithmic approach relevant to the problem's requirements.

---
+++ Daily Questions/Easy/FindMinimumOperationstoMakeAllElementsDivisiblebyThree_3190.cpp
@@ -0,0 +1,8 @@
+//Question 577
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        return count_if(nums.begin(), nums.end(), [&](int x){
+            return x%3!=0; });
+    }
+};
