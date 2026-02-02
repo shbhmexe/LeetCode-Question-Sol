31c8ec0b3553d282a3e057b0859918b3d5f44164
shbhmexe <shubhushukla586@gmail.com>
2026-02-02 22:19:21 +0530

    Add solutions for LeetCode questions 744, 3010, 3013, 2977
    Added C++ implementations for 'Divide an Array Into Subarrays With Minimum Cost I' (3010), 'Find Smallest Letter Greater Than Target' (744), 'Divide an Array Into Subarrays With Minimum Cost II' (3013), and 'Minimum Cost to Convert String II' (2977) in the Daily Questions directory. Each file contains the respective algorithmic approach and code solution.

---
+++ Daily Questions/Easy/DivideanArrayIntoSubarraysWithMinimumCostI_3010.cpp
@@ -0,0 +1,25 @@
+//Question 716
+//Approach (Just find minimums)
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+        int n = nums.size();
+
+        int firstMin  = nums[0];
+        int secondMin = INT_MAX;
+        int thirdMin  = INT_MAX;
+
+        for(int i = 1; i < n; i++) {
+            if(nums[i] < secondMin) {
+                thirdMin  = secondMin;
+                secondMin = nums[i];
+            } else if(nums[i] < thirdMin) {
+                thirdMin = nums[i];
+            }
+        }
+
+        return firstMin + secondMin + thirdMin;
+    }
+};
